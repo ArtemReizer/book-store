dae47f65eefeb0683efe7a0075908fd9a728ee98
Artem Reizer <artem1707200290@gmail.com>
2023-09-16 20:41:30 +0300

    added category model, repository and liquibase for category

---
+++ src/main/java/com/app/bookstore/model/Category.java
@@ -0,0 +1,21 @@
+package com.app.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    private String description;
+}
