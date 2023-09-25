6c331fce45a14c46376005671946c64eefd76d12
Artem Reizer <artem1707200290@gmail.com>
2023-09-25 17:39:03 +0300

    corrected compilation error

--- src/main/java/com/app/bookstore/controller/BookController.java
+++ src/main/java/com/app/bookstore/controller/BookController.java
@@ -1,17 +1,22 @@
 package com.app.bookstore.controller;
 
 import com.app.bookstore.dto.BookDto;
+import com.app.bookstore.dto.BookSearchParametersDto;
 import com.app.bookstore.dto.CreateBookRequestDto;
 import com.app.bookstore.service.BookService;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
