a66180ffc6f510350c120532a7d9a132fddfc569
Artem Reizer <artem1707200290@gmail.com>
2023-09-25 22:08:27 +0300

    corrected compilation error

--- src/main/java/com/app/bookstore/mapper/BookMapper.java
+++ src/main/java/com/app/bookstore/mapper/BookMapper.java
@@ -1,12 +1,12 @@
 package com.app.bookstore.mapper;
 
-import com.app.bookstore.config.MapperConfiguration;
+import com.app.bookstore.config.MapperConfig;
 import com.app.bookstore.dto.BookDto;
 import com.app.bookstore.dto.CreateBookRequestDto;
 import com.app.bookstore.model.Book;
 import org.mapstruct.Mapper;
 
-@Mapper(config = MapperConfiguration.class)
+@Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
