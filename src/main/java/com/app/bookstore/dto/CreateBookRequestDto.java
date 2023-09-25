5f3e5e04b306591d5072850361a72fc0b134da43
Artem Reizer <artem1707200290@gmail.com>
2023-09-25 17:21:08 +0300

    added constraints and created custom response class

--- src/main/java/com/app/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/app/bookstore/dto/CreateBookRequestDto.java
@@ -2,6 +2,7 @@ package com.app.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
@@ -9,8 +10,10 @@ import org.hibernate.validator.constraints.ISBN;
 @Data
 public class CreateBookRequestDto {
     @NotNull
+    @Size(min = 1, max = 255)
     private String title;
     @NotNull
+    @Size(min = 6, max = 255)
     private String author;
     @NotNull
     @ISBN
